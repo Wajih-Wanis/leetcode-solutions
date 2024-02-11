97745b20a1aeae51885622652c6344b045ef5788
Wajih-Wanis <wajihwanis2000@gmail.com>
2024-02-11 17:09:38 +0100

    Added design add and search words data structure

---
+++ 211. Design Add and Search Words Data Structure.py
@@ -0,0 +1,43 @@
+"""
+The intuition for this solution is that a word could be presented as a tree. More likely if for example word: bad and bake would have
+a common root which 'ba' and thus building this data structure would allow us to navigate the whole tree to either look for the existance
+of a full word or the pattern given by points. Of course the first thing that would come to the mind for this travesal that it should be 
+a dfs traversal. 
+"""
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+        
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()      
+
+    def addWord(self, word):
+        current_node = self.root
+        for character in word:
+            current_node = current_node.children.setdefault(character, TrieNode())
+        current_node.is_word = True
+        
+    def search(self, word):
+        def dfs(node, index):
+            if index == len(word):
+                return node.is_word
+               
+            if word[index] == ".":
+                for child in node.children.values():
+                    if dfs(child, index+1):
+                        return True
+                    
+            if word[index] in node.children:
+                return dfs(node.children[word[index]], index+1)
+            
+            return False
+    
+        return dfs(self.root, 0)
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
