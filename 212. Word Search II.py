a254dea4988f033d9046fa2bb34ace96cc984817
Wajih-Wanis <wajihwanis2000@gmail.com>
2024-02-12 21:52:51 +0100

    Added search words II

---
+++ 212. Word Search II.py
@@ -0,0 +1,38 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+    def addWord(self,word):
+        cur = self
+        for w in word:
+            if w not in cur.children:
+                cur.children[w] = TrieNode()
+            cur = cur.children[w]
+        cur.isWord = True
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        m,n = len(board),len(board[0])
+        root = TrieNode()
+        for w in words:
+            root.addWord(w)
+        
+        visited, res = set(),set()
+        def dfs(word,node,r,c):
+            if (r<0 or c<0 or r==m or c==n or not(board[r][c]  in node.children) or (r,c) in visited):
+                return 
+            visited.add((r,c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.isWord:
+                res.add(word)
+            directions = [[1,0],[-1,0],[0,1],[0,-1]]
+            for i,j in directions:
+                dfs(word, node, r+i,c+j)
+            visited.remove((r,c))
+
+        for i in range(m):
+            for j in range(n):
+                dfs("",root,i,j)
+
+        return list(res)
