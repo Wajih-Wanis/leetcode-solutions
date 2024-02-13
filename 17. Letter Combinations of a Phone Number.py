3cd56dcd5b087abcd7644fe58a1570077b481e9f
Wajih-Wanis <wajihwanis2000@gmail.com>
2024-02-13 21:09:44 +0100

    Added letter combinations of a phone number

---
+++ 17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,25 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":
+            return []
+        result = []
+        digitChar = {
+            "2":"abc",
+            "3":"def",
+            "4":"ghi",
+            "5":"jkl",
+            "6":"mno",
+            "7":"pqrs",
+            "8":"tuv",
+            "9":"wxyz"
+        }
+        def backtrack(i, seq):
+            if i==lastLevel:
+                result.append(seq)
+                return
+            for c in digitChar[digits[i]]:
+                backtrack(i+1,seq+c)
+        lastLevel = len(digits)
+        i = 0
+        backtrack(i,"")
+        return result
