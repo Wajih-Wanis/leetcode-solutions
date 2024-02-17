f2e6a0dca0094f2a52cc06e5817eead062727409
Wajih-Wanis <wajihwanis2000@gmail.com>
2024-02-17 21:06:05 +0100

    Added word ladder

---
+++ 127. Word Ladder.py
@@ -0,0 +1,23 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        visited = set()
+        def similarity(str1,str2):
+            i = len(str1)
+            for j in range(len(str1)):
+                if str1[j]==str2[j]:
+                    i-=1
+            return i
+        wordList = set(wordList)
+        queue = deque()
+        queue.append([beginWord,0])
+        visited.add(beginWord)
+        while queue: 
+            word, seq = queue.popleft()
+            if word == endWord:
+                    return seq+1
+            for w in wordList:
+                if similarity(word,w) == 1 and not(w in visited):
+                    queue.append([w,seq+1])
+                    visited.add(w)
+            wordList = wordList.difference(visited)
+        return 0
