2a28c21430ef503c5bc94da425ebbb04dba68758
Wajih <wajihwanis2000@gmail.com>
2024-03-08 16:55:41 +0100

    Added longest increasing subsequence

---
+++ 300. Longest Increasing Subsequence.py
@@ -0,0 +1,15 @@
+#The intuition is to use a dp array to get the comparaison between the 
+#length of the current subsequence and the max subsequence at that slot
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        dp = [1] * len(nums)
+
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return max(dp)
